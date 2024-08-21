b6516713203fe553a9e4c8fc57bd6259769abe61
Md-Jahirul-Islam-56 <jahirulrazon0@gmail.com>
2024-08-21 21:29:47 +0600

    Add files via upload

---
+++ Adruino Programming/RGB_LED/RGB_LED.ino
@@ -0,0 +1,29 @@
+// C++ code
+//
+void setup()
+{
+   pinMode(13, OUTPUT);
+   pinMode(12, OUTPUT);
+   pinMode(8, OUTPUT);
+  
+}
+
+void loop()
+{
+  //By mixing rgb colors we can find different other colors
+  //We can also find other colors by changing the resistance values as it allows some percentage of current through those rgb colors
+  
+  digitalWrite(13, HIGH);
+ // delay(1000); 
+// digitalWrite(13, LOW);
+  //delay(1000); 
+   digitalWrite(12, HIGH);
+ // delay(1000); 
+  //digitalWrite(12, LOW);
+  //delay(1000);
+ //  digitalWrite(8, HIGH);
+ // delay(1000); 
+ // digitalWrite(8, LOW);
+ // delay(1000);
+  
+}
